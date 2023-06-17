2a53ca242370f7bc613bf6b74826e525ab86ab84
Zakhar <zox_13@mail.ru>
2023-06-17 06:21:54 +0300

    Tests complete. Now start classes

--- functions1.py
+++ functions1.py
@@ -1,30 +1,33 @@
 PATH = r'C:\Users\Zox\Desktop\IT Future\5_python\phonebook_python\data.txt'
 import text
-def create_func():
+
+def create_func() ->None:
     contact = []
     contact.append(input(text.qn_creating_1))
     contact.append(input(text.qn_creating_2))
     contact.append(input(text.qn_creating_3))
     with open (PATH, 'a', encoding="utf-8") as database:
-        database.write(str(contact)+"\n")
-def search_func()->None:
-    with open (PATH, 'r', encoding='utf-8') as database:
-        name = input(text.search_header)
-        file = (database.readlines())
-        flag = True
-        for index, contact_row in enumerate(file):
-            if name.lower() in contact_row.lower():
-                print(index, '. ', contact_row)
-                flag = False
-        if flag:
-            print(text.find_nothing)
-            return search_func()
+        database.write(str(contact)+"\n") #надо было джойнить, не было бы барахла рядом со значениями
+def search_func()->None: 
+    while True:
+        with open (PATH, 'r', encoding='utf-8') as database:
+            name = input(text.search_header)
+            file = (database.readlines())
+            flag = True
+            for index, contact_row in enumerate(file):
+                if name.lower() in contact_row.lower():
+                    print(index, '. ', contact_row) #выводит некрасиво, сделать как 27,28 строки + шапка таблицы.
+                                                    #!лучше не принтить в теле, а добавлять в словарь(например).и словарь на ретерн
+                    flag = False
+            if not flag:
+                break
+        print(text.find_nothing)
 def contact_card(index: int) ->None:
     with open (PATH, 'r', encoding='utf-8') as database:
         file = (database.readlines())
         # print(file[index]) 
         for i in file[index].split(', '):
-            print(f'{i.strip(): ^20}', end=' ') #strip не работает
+            print(f'{i.strip(): ^20}', end=' ')
         print()
 
 def call_func():
@@ -38,21 +41,18 @@ def delete_func(index: int):
 def watch_all():
     with open (PATH, 'r', encoding='utf-8') as database:
         file = (database.readlines())
-        # for i, j in enumerate(file):
-        #     print(i, '. ', j)
         for i, j in enumerate(file):
             print(i, ". ", end = '')
             for k in j.split(","):
                 print(f'{k: ^20}', end='') #надо стрипануть
-            print() #попробуй закомментить и распечатать
+            print()
 
-
-def edit_func(index: int):
+def edit_func(index: int): #это незаконно, научиться вытаскивать конкретный элемент контакта, переделать
     create_func()
     delete_func(index)
 
 def input_contact_menu():
-    choise_contact_menu = int(-1)
+    choise_contact_menu = -1
     while 1 > choise_contact_menu or choise_contact_menu > 4:
         choise_contact_menu = int(input(text.contact_menu))
         return choise_contact_menu
--- ui.py
+++ ui.py
@@ -2,34 +2,34 @@ import functions1
 import text
 
 def start():
-    choise = int(-1)
-    while 1 > choise or choise > 4:
-        choise = int(input(text.main_menu))
-        if choise == 1:
-            functions1.search_func()
-            print(text.qn_contact)
-            index = int(input())
-            print(text.contact_card_header)
-            print(text.table_info)
-            functions1.contact_card(index)
-            choise_contact_menu = functions1.input_contact_menu()
-            if choise_contact_menu == 1:
-                functions1.call_func()
-            elif choise_contact_menu == 2:
-                functions1.edit_func(index)
-                print(text.editing_complete)
-            elif choise_contact_menu == 3:
-                functions1.delete_func(index)
-                print(text.deleting_complete)
-            else:
-                pass
-        elif choise == 2:
-            functions1.create_func()
-            print(text.creating_complete)
-        elif choise == 3:
-            print(text.watch_all_header)
-            print(text.table_info)
-            functions1.watch_all()
-        if choise == 4:
-            break
-        start() 
+    while True:
+        choise = -1
+        while 1 > choise or choise > 4:
+            choise = int(input(text.main_menu))
+            if choise == 1:
+                functions1.search_func()
+                print(text.qn_contact)
+                index = int(input())
+                print(text.contact_card_header)
+                print(text.table_info)
+                functions1.contact_card(index)
+                choise_contact_menu = functions1.input_contact_menu()
+                if choise_contact_menu == 1:
+                    functions1.call_func()
+                elif choise_contact_menu == 2:
+                    functions1.edit_func(index)
+                    print(text.editing_complete)
+                elif choise_contact_menu == 3:
+                    functions1.delete_func(index)
+                    print(text.deleting_complete)
+                else:
+                    pass
+            elif choise == 2:
+                functions1.create_func()
+                print(text.creating_complete)
+            elif choise == 3:
+                print(text.watch_all_header)
+                print(text.table_info)
+                functions1.watch_all()
+            if choise == 4:
+                exit()
